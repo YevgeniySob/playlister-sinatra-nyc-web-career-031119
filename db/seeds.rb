b3c3c2c2a6a9cd3809afeed67924c89013e05101
Yevgeniy <yevgeniyerot@gmail.com>
2019-04-03 11:51:24 -0400

    setting up process

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,18 @@
 # Add seed data here. Seed your database with `rake db:seed`
+#
+# artist1 = Artist.find_or_create_by(name: 'Yev')
+# artist2 = Artist.find_or_create_by(name: 'Caridad')
+# artist3 = Artist.find_or_create_by(name: 'Test1')
+# artist4 = Artist.find_or_create_by(name: 'MyAlterEgo')
+#
+# song1 = Song.find_or_create_by(name: 'In The End')
+# song2 = Song.find_or_create_by(name: 'Numb')
+# song3 = Song.find_or_create_by(name: 'Oh yeeeeh')
+# song4 = Song.find_or_create_by(name: 'Love')
+#
+# genre1 = Genre.find_or_create_by(name: 'Alternative')
+# genre2 = Genre.find_or_create_by(name: 'R&B')
+# genre3 = Genre.find_or_create_by(name: 'Pop')
+# genre4 = Genre.find_or_create_by(name: 'Rock')
 
+LibraryParser.parse
